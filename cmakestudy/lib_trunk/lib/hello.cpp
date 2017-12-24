7aeecfa142a1cc132e634423c5277ac8bb0d61ea
eaikao <279712302@qq.com>
2017-12-24 23:29:58 +0800

    cmakelist study capture 5

---
+++ cmakestudy/lib_trunk/lib/hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: eaikao
+    > Mail: 279712302@qq.com 
+    > Created Time: 2017年12月24日 星期日 22时52分07秒
+ ************************************************************************/
+#include <stdio.h>
+#include <iostream>
+#include "hello.h"
+
+void HelloFunc()
+{
+	printf("hello world\n");
+}
